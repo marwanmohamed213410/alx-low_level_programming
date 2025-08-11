d0a9a497239809f0db6023f612244075077c1e2b
marwan <marwanmohamed2238@gmail.com>
2025-08-12 00:37:26 +0300

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar (i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
