508f19183fee5a7494014e22858e5843e6398a28
marwan <marwanmohamed2238@gmail.com>
2025-08-12 15:09:11 +0300

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,12 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 9)
+		putchar(i);
+		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		putchar(i);
 	}
 	putchar('\n');
 	return (0);
