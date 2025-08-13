af425faccd660960be2f4eecc9023a15092bda52
marwan <marwanmohamed2238@gmail.com>
2025-08-13 14:53:06 +0300

    make betty happy

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,16 @@
 
 int main(void)
 {
-	int i,j;
-	for(i = 0; i <= 8; i++)
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
 	{
-		for(j = i+1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			
-			if(i != 8 || j != 9)
+
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
