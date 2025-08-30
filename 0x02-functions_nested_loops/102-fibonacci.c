a4b355ac801d3145a51d9d665186970ee72788c6
marwan <marwanmohamed2238@gmail.com>
2025-08-30 17:49:09 +0300

    make betty happy

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - etry point
+ *
+ * Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0 (success)
+*/
+int main(void)
 {
-    int num1 = 1, num2 = 2, nextNum = 0, i;
+	int num1 = 1, num2 = 2, nextNum = 0, i;
 
-    for (i = 1; i <= 50; i++)
-    {
-        if (i > 3)
-        {
-            nextNum = num1 + num2;
-            num1 = num2;
-            num2 = nextNum;
-            printf("%d", nextNum);
-        }
-        else if (i == 1)
-            printf("%d", num1);
-        else if (i == 2)
-            printf("%d", num2);
-        
-        if (i == 49)
+	for (i = 1; i <= 50; i++)
+	{
+		if (i > 3)
+		{
+			nextNum = num1 + num2;
+			num1 = num2;
+			num2 = nextNum;
+			printf("%d", nextNum);
+		}
+		else if (i == 1)
+			printf("%d", num1);
+		else if (i == 2)
+			printf("%d", num2);
+
+		if (i == 49)
 			printf("\n");
 		else
 			printf(", ");
-    }
-    return (0);
+	}
+	return (0);
 }
