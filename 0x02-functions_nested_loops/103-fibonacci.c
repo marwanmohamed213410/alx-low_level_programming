18f7f924d69b88d8bd558b1f360da7fd6bfdfb58
marwan <marwanmohamed2238@gmail.com>
2025-08-30 20:58:58 +0300

    finds and prints the sum of the even-valued terms, followed by a new line

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: finds and prints the sum of the even-valued terms
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	unsigned long num1 = 1, num2 = 2, nextNum;
+	unsigned long sum = 0;
+
+	sum += num2;
+
+	while (1)
+	{
+		printf("%lu, ", num1);
+		printf("%lu, ", num2);
+
+		nextNum = num1 + num2;
+		printf("%lu, ", nextNum);
+
+		num1 = num2;
+		num2 = nextNum;
+
+		if (nextNum > 4000000)
+			break;
+		if ((nextNum % 2) == 0)
+			sum += nextNum;
+
+		num1 = num2;
+		num2 = nextNum;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
